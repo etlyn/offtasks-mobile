a440214fa1dfa000f957ca7b2e66e4168db888c1
Giorgi Digmelashvili <128441413+etlyn@users.noreply.github.com>
2026-09-11 18:56:23 -0700

    chore: refresh iOS dependency lock and format Babel configuration

--- babel.config.js
+++ babel.config.js
@@ -1,21 +1,26 @@
 module.exports = api => ({
   presets: ['module:@react-native/babel-preset'],
   plugins: [
-    ...(api.env('test') ? [] : [[
-      'module:react-native-dotenv',
-      {
-        moduleName: '@env',
-        path: '.env',
-        allowUndefined: true,
-      },
-    ]]),
+    ...(api.env('test')
+      ? []
+      : [
+          [
+            'module:react-native-dotenv',
+            {
+              moduleName: '@env',
+              path: '.env',
+              allowUndefined: true,
+            },
+          ],
+        ]),
     [
       'module-resolver',
       {
         root: ['./src'],
         alias: {
           '@': './src',
-          'react-native-vector-icons/Feather': './src/components/icons/FeatherIcon',
+          'react-native-vector-icons/Feather':
+            './src/components/icons/FeatherIcon',
         },
       },
     ],
